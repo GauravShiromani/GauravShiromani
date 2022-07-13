5f070ae6ce1fb15e87853d4bb730de1ad175e7cc
/ɢᴀᴜʀᴀᴠ sʜɪʀᴏᴍᴀɴɪ <109208213+GauravShiromani@users.noreply.github.com>
2022-07-13 15:43:42 +0530

    Add files via upload

---
+++ Docs/Python/class 1/practice1.py
@@ -0,0 +1,6 @@
+import time
+# import flask
+'''author gaurav
+date 16 feb'''
+time.sleep (2)
+print("sleep for 2 second")
---
+++ Docs/Python/class 1/practice3.py
@@ -0,0 +1,4 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("hey preety i am your python module. what are you doing")
+engine.runAndWait()
